f840f6a41350f48479274fe7656877e46452a668
Pinchuk Dmytro <pinchukdmitro22@gmail.com>
2026-08-22 20:21:35 +0300

    дз 2

---
+++ src/routes/notesRoutes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import {
+  getAllNotes,
+  getNoteById,
+  createNote,
+  updateNote,
+  deleteNote,
+} from '../controllers/notesController.js';
+const notesRouter = Router();
+notesRouter.get('/notes', getAllNotes);
+notesRouter.get('/notes/:noteId', getNoteById);
+notesRouter.post('/notes', createNote);
+notesRouter.patch('/notes/:noteId', updateNote);
+notesRouter.delete('/notes/:noteId', deleteNote);
+export default notesRouter;
