04d144927448fc4a3198b05283d45433ce7acebe
Pinchuk Dmytro <pinchukdmitro22@gmail.com>
2026-09-06 20:14:29 +0300

    микроисправление

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import dns from 'node:dns';
 dns.setServers(['8.8.8.8', '8.8.4.4']);
-import 'dotenv/config';
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import {connectMongoDB} from './db/connectMongoDB.js';
