9bfe8d0266742346198585ba195de601c65d2011
Pinchuk Dmytro <pinchukdmitro22@gmail.com>
2026-09-13 23:55:25 +0300

    исправление в мультере

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,16 +1,13 @@
 import multer from 'multer';
 export const upload = multer({
   storage: multer.memoryStorage(),
-  limits: { fileSize: 2 * 1024 * 1024 },
+
+  limits: {
+    fileSize: 2 * 1024 * 1024,
+  },
+
   fileFilter: (req, file, cb) => {
-    const allowedTypes = [
-      'image/jpeg',
-      'image/jpg',
-      'image/png',
-      'image/gif',
-      'image/webp',
-    ];
-    if (allowedTypes.includes(file.mimetype)) {
+    if (file.mimetype.startsWith('image/')) {
       cb(null, true);
     } else {
       cb(new Error('Only images allowed'), false);
