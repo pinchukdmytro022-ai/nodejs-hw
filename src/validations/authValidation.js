762c69ec75f7fae8900d927019e89173cab1d3b8
Pinchuk Dmytro <pinchukdmitro22@gmail.com>
2026-09-13 17:23:11 +0300

    оновлення валідації та роуту

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -12,3 +12,16 @@ export const loginUserSchema = {
     password: Joi.string().required(),
   })
 };
+
+export const requestResetEmailSchema = {
+  body: Joi.object({
+    email: Joi.string().email().required(),
+  }),
+};
+
+export const resetPasswordSchema = {
+  body: Joi.object({
+    token: Joi.string().required(),
+    password: Joi.string().min(8).required(),
+  }),
+};
